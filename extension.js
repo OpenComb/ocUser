02a092c2ff9f96ed5fd5648aa623fa54c0f89f27
aleechou <zq@alee2002.com>
2013-06-15 10:16:26 +0800

    add controlpanel size menu item

--- extension.js
+++ extension.js
@@ -52,19 +52,34 @@ exports.onload = function(app){
     ) ;
 
     //插入 view
-		helper.log.trace("befor load template: ocframework/templates/WebLayout.html") ;
-	    helper.template("ocframework/templates/WebLayout.html",this.hold(function(err,tpl){
+    helper.template("ocframework/templates/WebLayout.html",this.hold(function(err,tpl){
 
-	        if(err) throw err ;
+        if(err) throw err ;
 
-		    helper.log.trace("before template weave") ;
-	        tpl.$(".header-right").prepend('<div class="header-userpad" style="float:right"><view name="userpad" /></div>') ;
+	    helper.log.trace("before template weave") ;
+        tpl.$(".header-right").prepend('<div class="header-userpad" style="float:right"><view name="userpad" /></div>') ;
 
-		    console.log(tpl.$(".header-userpad").html()) ;
+	    // 重新编译模板
+	    tpl.compile() ;
+    })) ;
 
-		    // 重新编译模板
-		    tpl.compile() ;
-	    })) ;
+	// control panel menu items
+	helper.template("ocframework/templates/ControlPanel.html",this.hold(function(err,tpl){
+
+		if(err) throw err ;
+
+		tpl.$("#sidemenu").prepend(
+			'<li id="sidemenu-item-user">'
+				+ '<h3>用户</h3>'
+				+ '<ul class="toggle">'
+					+ '<li><i class="icon-group"></i> <a class="stay" href="/ocuser/UserList">浏览用户</a></li>'
+				+ '</ul>'
+			+ '</li>'
+		) ;
+
+		tpl.compile() ;
+	})) ;
+
 
     // 注册控制器的别名
     Controller.registerAlias('signin','ocuser/lib/SignIn.js') ;
